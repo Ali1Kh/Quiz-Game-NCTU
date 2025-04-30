741fb35088a0a940c938f9b62adb73e91f3c4333
Ali Khaled <alielsaadany94@gmail.com>
2025-05-01 00:56:08 +0300

    ui

--- src/main.c
+++ src/main.c
@@ -2,196 +2,175 @@
 #include <commctrl.h>
 #include <stdio.h>
 
-#define ID_CATEGORY 1
-#define ID_QUESTIONS 2
-#define ID_DIFFICULTY 3
-#define ID_START_BTN 4
-#define ID_ANSWER1 5
-#define ID_ANSWER2 6
-#define ID_ANSWER3 7
-#define ID_ANSWER4 8
-#define ID_NEXT_BTN 9
-#define ID_SCORE_LABEL 10
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "gdi32.lib")
 
-// Declare these variables globally
-HWND hCategory, hDifficulty, hQuestions, hStartBtn, hAnswer1, hAnswer2, hAnswer3, hAnswer4, hNextBtn, hScoreLabel;
+#define ID_START      1
+#define ID_CATEGORY1  2
+#define ID_CATEGORY2  3
+#define ID_EASY       4
+#define ID_MEDIUM     5
+#define ID_HARD       6
+#define ID_ANSWER1    7
+#define ID_ANSWER2    8
+#define ID_ANSWER3    9
+#define ID_ANSWER4    10
+#define ID_NEXT       11
 
-// Define some dummy questions and answers
-typedef struct
-{
-    char question[256];
-    char answers[4][256];
-    int correctAnswer;
-} Question;
-
-Question questions[] = {
-    {"What is the capital of France?", {"Paris", "London", "Berlin", "Madrid"}, 0},
-    {"Which planet is known as the Red Planet?", {"Earth", "Mars", "Jupiter", "Saturn"}, 1},
-    {"Who wrote 'Romeo and Juliet'?", {"Shakespeare", "Dickens", "Hemingway", "Fitzgerald"}, 0},
-    {"What is the largest ocean on Earth?", {"Atlantic", "Indian", "Pacific", "Arctic"}, 2}};
-
+HWND hCategory1, hCategory2, hEasy, hMedium, hHard;
+HWND hStartBtn, hQuestionText, hAnswer1, hAnswer2, hAnswer3, hAnswer4, hNextBtn, hScoreLabel;
 int currentQuestion = 0;
 int score = 0;
 
-LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
-void ShowQuestion(HWND hwnd);
+const char* questions[] = {
+    "What is the capital of France?",
+    "Which language is used for system programming?",
+    "What does RAM stand for?"
+};
 
-int main()
-{
-    // Initialize common controls
-    INITCOMMONCONTROLSEX icex;
-    icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
-    icex.dwICC = ICC_LISTVIEW_CLASSES;
-    InitCommonControlsEx(&icex);
+const char* answers[][4] = {
+    {"Paris", "London", "Berlin", "Rome"},
+    {"Python", "C", "HTML", "PHP"},
+    {"Random Access Memory", "Read Access Memory", "Run Accept Memory", "Ready Auto Machine"}
+};
 
-    // Window class setup
-    const char g_szClassName[] = "QuizGameClass";
+const int correctAnswers[] = {0, 1, 0};
+
+int selectedAnswer = -1;
+
+HBRUSH hBlueBrush, hBackgroundBrush;
+
+void ShowQuestion(HWND hwnd) {
+    if (currentQuestion >= 3) {
+        char result[50];
+        sprintf(result, "Your Score: %d / 3", score);
+        SetWindowText(hScoreLabel, result);
+        ShowWindow(hScoreLabel, SW_SHOW);
+        ShowWindow(hAnswer1, SW_HIDE);
+        ShowWindow(hAnswer2, SW_HIDE);
+        ShowWindow(hAnswer3, SW_HIDE);
+        ShowWindow(hAnswer4, SW_HIDE);
+        ShowWindow(hNextBtn, SW_HIDE);
+        return;
+    }
+
+    SetWindowText(hQuestionText, questions[currentQuestion]);
+    SetWindowText(hAnswer1, answers[currentQuestion][0]);
+    SetWindowText(hAnswer2, answers[currentQuestion][1]);
+    SetWindowText(hAnswer3, answers[currentQuestion][2]);
+    SetWindowText(hAnswer4, answers[currentQuestion][3]);
+
+    CheckRadioButton(hwnd, ID_ANSWER1, ID_ANSWER4, 0);
+    selectedAnswer = -1;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch(msg) {
+        case WM_CREATE:
+            hBlueBrush = CreateSolidBrush(RGB(30, 144, 255));
+            hBackgroundBrush = CreateSolidBrush(RGB(245, 245, 245));
+
+            CreateWindow("STATIC", "Choose Category:", WS_VISIBLE | WS_CHILD,
+                20, 20, 150, 20, hwnd, NULL, NULL, NULL);
+            hCategory1 = CreateWindow("BUTTON", "General", WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                20, 50, 100, 20, hwnd, (HMENU)ID_CATEGORY1, NULL, NULL);
+            hCategory2 = CreateWindow("BUTTON", "Tech", WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                130, 50, 100, 20, hwnd, (HMENU)ID_CATEGORY2, NULL, NULL);
+
+            CreateWindow("STATIC", "Choose Difficulty:", WS_VISIBLE | WS_CHILD,
+                20, 90, 150, 20, hwnd, NULL, NULL, NULL);
+            hEasy = CreateWindow("BUTTON", "Easy", WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                20, 120, 100, 20, hwnd, (HMENU)ID_EASY, NULL, NULL);
+            hMedium = CreateWindow("BUTTON", "Medium", WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                130, 120, 100, 20, hwnd, (HMENU)ID_MEDIUM, NULL, NULL);
+            hHard = CreateWindow("BUTTON", "Hard", WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                240, 120, 100, 20, hwnd, (HMENU)ID_HARD, NULL, NULL);
+
+            hStartBtn = CreateWindow("BUTTON", "Start Quiz", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                100, 160, 120, 30, hwnd, (HMENU)ID_START, NULL, NULL);
+
+            hQuestionText = CreateWindow("STATIC", "", WS_CHILD | SS_CENTER,
+                20, 210, 400, 30, hwnd, NULL, NULL, NULL);
+            hAnswer1 = CreateWindow("BUTTON", "", WS_CHILD | BS_AUTORADIOBUTTON,
+                20, 250, 400, 20, hwnd, (HMENU)ID_ANSWER1, NULL, NULL);
+            hAnswer2 = CreateWindow("BUTTON", "", WS_CHILD | BS_AUTORADIOBUTTON,
+                20, 280, 400, 20, hwnd, (HMENU)ID_ANSWER2, NULL, NULL);
+            hAnswer3 = CreateWindow("BUTTON", "", WS_CHILD | BS_AUTORADIOBUTTON,
+                20, 310, 400, 20, hwnd, (HMENU)ID_ANSWER3, NULL, NULL);
+            hAnswer4 = CreateWindow("BUTTON", "", WS_CHILD | BS_AUTORADIOBUTTON,
+                20, 340, 400, 20, hwnd, (HMENU)ID_ANSWER4, NULL, NULL);
+            hNextBtn = CreateWindow("BUTTON", "Next", WS_CHILD,
+                170, 380, 100, 30, hwnd, (HMENU)ID_NEXT, NULL, NULL);
+
+            hScoreLabel = CreateWindow("STATIC", "", WS_CHILD | SS_CENTER,
+                20, 420, 400, 30, hwnd, NULL, NULL, NULL);
+            break;
+
+        case WM_COMMAND:
+            switch(LOWORD(wParam)) {
+                case ID_START:
+                    ShowWindow(hQuestionText, SW_SHOW);
+                    ShowWindow(hAnswer1, SW_SHOW);
+                    ShowWindow(hAnswer2, SW_SHOW);
+                    ShowWindow(hAnswer3, SW_SHOW);
+                    ShowWindow(hAnswer4, SW_SHOW);
+                    ShowWindow(hNextBtn, SW_SHOW);
+                    ShowWindow(hCategory1, SW_HIDE);
+                    ShowWindow(hCategory2, SW_HIDE);
+                    ShowWindow(hEasy, SW_HIDE);
+                    ShowWindow(hMedium, SW_HIDE);
+                    ShowWindow(hHard, SW_HIDE);
+                    ShowWindow(hStartBtn, SW_HIDE);
+                    ShowQuestion(hwnd);
+                    break;
+
+                case ID_ANSWER1: selectedAnswer = 0; break;
+                case ID_ANSWER2: selectedAnswer = 1; break;
+                case ID_ANSWER3: selectedAnswer = 2; break;
+                case ID_ANSWER4: selectedAnswer = 3; break;
+
+                case ID_NEXT:
+                    if (selectedAnswer == correctAnswers[currentQuestion])
+                        score++;
+
+                    currentQuestion++;
+                    ShowQuestion(hwnd);
+                    break;
+            }
+            break;
+
+        case WM_CTLCOLORSTATIC:
+        case WM_CTLCOLORBTN:
+            return (LRESULT)hBackgroundBrush;
+
+        case WM_DESTROY:
+            DeleteObject(hBlueBrush);
+            DeleteObject(hBackgroundBrush);
+            PostQuitMessage(0);
+            break;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASS wc = {0};
-    wc.lpfnWndProc = WindowProcedure;
-    wc.hInstance = GetModuleHandle(NULL);
-    wc.lpszClassName = g_szClassName;
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszClassName = "QuizGameClass";
 
-    if (!RegisterClass(&wc))
-    {
-        MessageBox(NULL, "Window class registration failed!", "Error", MB_ICONERROR | MB_OK);
-        return 1;
-    }
+    RegisterClass(&wc);
 
-    // Create the window
-    HWND hwnd = CreateWindow(g_szClassName, "Quiz Game",
-                             WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-                             400, 300, NULL, NULL, wc.hInstance, NULL);
-    if (hwnd == NULL)
-    {
-        MessageBox(NULL, "Window creation failed!", "Error", MB_ICONERROR | MB_OK);
-        return 1;
-    }
+    HWND hwnd = CreateWindow("QuizGameClass", "Quiz Game - NCTU", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 480, 520, NULL, NULL, hInstance, NULL);
 
-    // Show and update window
-    ShowWindow(hwnd, SW_SHOW);
+    ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    // Main message loop
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
+    MSG msg = {0};
+    while(GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
     return msg.wParam;
-}
-
-LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-{
-    switch (msg)
-    {
-    case WM_CREATE:
-    {
-        // Category combo box
-        hCategory = CreateWindow("COMBOBOX", "", WS_CHILD | WS_VISIBLE | CBS_DROPDOWN, 50, 50, 120, 200, hwnd, (HMENU)ID_CATEGORY, NULL, NULL);
-        SendMessage(hCategory, CB_ADDSTRING, 0, (LPARAM) "Science");
-        SendMessage(hCategory, CB_ADDSTRING, 0, (LPARAM) "History");
-        SendMessage(hCategory, CB_ADDSTRING, 0, (LPARAM) "Geography");
-
-        // Number of questions spinner
-        hQuestions = CreateWindow("EDIT", "5", WS_CHILD | WS_VISIBLE | WS_BORDER, 50, 100, 40, 20, hwnd, (HMENU)ID_QUESTIONS, NULL, NULL);
-
-        // Difficulty combo box
-        hDifficulty = CreateWindow("COMBOBOX", "", WS_CHILD | WS_VISIBLE | CBS_DROPDOWN, 50, 150, 120, 200, hwnd, (HMENU)ID_DIFFICULTY, NULL, NULL);
-        SendMessage(hDifficulty, CB_ADDSTRING, 0, (LPARAM) "Easy");
-        SendMessage(hDifficulty, CB_ADDSTRING, 0, (LPARAM) "Medium");
-        SendMessage(hDifficulty, CB_ADDSTRING, 0, (LPARAM) "Hard");
-
-        // Start button
-        hStartBtn = CreateWindow("BUTTON", "Start", WS_CHILD | WS_VISIBLE, 50, 200, 100, 30, hwnd, (HMENU)ID_START_BTN, NULL, NULL);
-        break;
-    }
-
-    case WM_COMMAND:
-    {
-        if (LOWORD(wp) == ID_START_BTN)
-        {
-            // Hide the initial setup controls
-            ShowWindow(hCategory, SW_HIDE);
-            ShowWindow(hDifficulty, SW_HIDE);
-            ShowWindow(hQuestions, SW_HIDE);
-            ShowWindow(hStartBtn, SW_HIDE);
-
-            // Show the question UI
-            ShowQuestion(hwnd);
-        }
-
-        if (LOWORD(wp) == ID_NEXT_BTN)
-        {
-            // Check the selected answer and update the score
-            if (IsDlgButtonChecked(hwnd, ID_ANSWER1) && questions[currentQuestion].correctAnswer == 0)
-            {
-                score++;
-            }
-            else if (IsDlgButtonChecked(hwnd, ID_ANSWER2) && questions[currentQuestion].correctAnswer == 1)
-            {
-                score++;
-            }
-            else if (IsDlgButtonChecked(hwnd, ID_ANSWER3) && questions[currentQuestion].correctAnswer == 2)
-            {
-                score++;
-            }
-            else if (IsDlgButtonChecked(hwnd, ID_ANSWER4) && questions[currentQuestion].correctAnswer == 3)
-            {
-                score++;
-            }
-
-            currentQuestion++;
-
-            // If there are no more questions, show the score
-            if (currentQuestion >= sizeof(questions) / sizeof(questions[0]))
-            {
-                char scoreMessage[100];
-                sprintf(scoreMessage, "Your score is: %d", score);
-                MessageBox(hwnd, scoreMessage, "Quiz Finished", MB_OK);
-                PostQuitMessage(0);
-            }
-            else
-            {
-                ShowQuestion(hwnd);
-            }
-        }
-        break;
-    }
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-
-    default:
-        return DefWindowProc(hwnd, msg, wp, lp);
-    }
-    return 0;
-}
-
-void ShowQuestion(HWND hwnd)
-{
-    // Remove existing controls if they exist
-    if (hAnswer1)
-        DestroyWindow(hAnswer1);
-    if (hAnswer2)
-        DestroyWindow(hAnswer2);
-    if (hAnswer3)
-        DestroyWindow(hAnswer3);
-    if (hAnswer4)
-        DestroyWindow(hAnswer4);
-    if (hNextBtn)
-        DestroyWindow(hNextBtn);
-
-    // Display the current question
-    CreateWindow("STATIC", questions[currentQuestion].question, WS_CHILD | WS_VISIBLE, 50, 50, 300, 40, hwnd, NULL, NULL, NULL);
-
-    // Display 4 answer options as radio buttons
-    hAnswer1 = CreateWindow("BUTTON", questions[currentQuestion].answers[0], WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 50, 100, 200, 30, hwnd, (HMENU)ID_ANSWER1, NULL, NULL);
-    hAnswer2 = CreateWindow("BUTTON", questions[currentQuestion].answers[1], WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 50, 140, 200, 30, hwnd, (HMENU)ID_ANSWER2, NULL, NULL);
-    hAnswer3 = CreateWindow("BUTTON", questions[currentQuestion].answers[2], WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 50, 180, 200, 30, hwnd, (HMENU)ID_ANSWER3, NULL, NULL);
-    hAnswer4 = CreateWindow("BUTTON", questions[currentQuestion].answers[3], WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 50, 220, 200, 30, hwnd, (HMENU)ID_ANSWER4, NULL, NULL);
-
-    // Next button
-    hNextBtn = CreateWindow("BUTTON", "Next", WS_CHILD | WS_VISIBLE, 50, 260, 100, 30, hwnd, (HMENU)ID_NEXT_BTN, NULL, NULL);
 }
