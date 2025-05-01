69661d2944cff4734778c0338e4ac285c0bb932f
Ali <alielsaadany94@gmail.com>
2025-05-01 23:02:43 +0300

    main ui

--- src/main.c
+++ src/main.c
@@ -1,54 +1,62 @@
 #include <gtk/gtk.h>
 
-void activate(GtkApplication *app, gpointer user_data) {
-    // Create the list models using GListStore (GTK4 recommended)
-    GListStore *category_list = g_list_store_new(G_TYPE_STRING);
-    GListStore *questions_list = g_list_store_new(G_TYPE_STRING);
-    GListStore *difficulty_list = g_list_store_new(G_TYPE_STRING);
-
-    // Add items to the list models
-    const gchar *categories[] = {"Math", "Science", "History"};
-    const gchar *questions[] = {"Q1", "Q2", "Q3"};
-    const gchar *difficulties[] = {"Easy", "Medium", "Hard"};
-
-    for (int i = 0; i < 3; i++) {
-        g_list_store_append(category_list, &categories[i]);
-        g_list_store_append(questions_list, &questions[i]);
-        g_list_store_append(difficulty_list, &difficulties[i]);
-    }
-
-    // Create drop-down widgets
-    GtkWidget *category_combo = gtk_drop_down_new(G_LIST_MODEL(category_list), NULL);
-    GtkWidget *questions_combo = gtk_drop_down_new(G_LIST_MODEL(questions_list), NULL);
-    GtkWidget *difficulty_combo = gtk_drop_down_new(G_LIST_MODEL(difficulty_list), NULL);
-
-    // Create a window to display the dropdowns
-    GtkWidget *window = gtk_window_new();  // No need to specify GTK_WINDOW_TOPLEVEL in GTK4
-    gtk_window_set_title(GTK_WINDOW(window), "Quiz App");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
-    
-    // Add the drop-down widgets to the window's layout
-    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_box_append(GTK_BOX(box), category_combo);
-    gtk_box_append(GTK_BOX(box), questions_combo);
-    gtk_box_append(GTK_BOX(box), difficulty_combo);
-    
-    gtk_window_set_child(GTK_WINDOW(window), box);
-
-    // Present the window
-    gtk_window_present(GTK_WINDOW(window));  // Replaces gtk_widget_show for windows
-
-    // Set the visibility for drop-downs
-    gtk_widget_set_visible(category_combo, TRUE);
-    gtk_widget_set_visible(questions_combo, TRUE);
-    gtk_widget_set_visible(difficulty_combo, TRUE);
+// Function to handle button click event
+static void on_button_clicked(GtkButton *button, gpointer user_data)
+{
+    g_print("Button clicked\n");
 }
 
-int main(int argc, char **argv) {
-    // Use G_APPLICATION_DEFAULT_FLAGS instead of deprecated G_APPLICATION_FLAGS_NONE
-    GtkApplication *app = gtk_application_new("com.example.quizapp", G_APPLICATION_DEFAULT_FLAGS);
+// Function to activate the application and display the window
+static void activate(GtkApplication *app, gpointer user_data)
+{
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *label;
+    GtkWidget *box;
+
+    // Create a new window
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Quiz Game");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+    // Create a vertical box to arrange widgets inside the window
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_window_set_child(GTK_WINDOW(window), box); // Use gtk_window_set_child instead of gtk_container_add
+
+    // Create a label and add it to the box
+    label = gtk_label_new("Welcome to the Quiz Game!");
+    gtk_box_append(GTK_BOX(box), label); // Use gtk_box_append instead of gtk_box_pack_start
+
+    // Create a button and add it to the box
+    button = gtk_button_new_with_label("Click Me");
+    gtk_box_append(GTK_BOX(box), button); // Use gtk_box_append instead of gtk_box_pack_start
+
+    // Connect the button's "clicked" signal to the event handler
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+    // Connect the "destroy" signal to quit the application when the window is closed
+    g_signal_connect(window, "destroy", G_CALLBACK(g_application_quit), app);
+
+    // Show the window
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char *argv[])
+{
+    GtkApplication *app;
+    int status;
+
+    // Create a new GTK application
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+
+    // Connect the "activate" signal to show the window
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    // Run the application
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    // Clean up resources
     g_object_unref(app);
+
     return status;
 }
